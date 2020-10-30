69ea09ed9dcc350a71f0e21c17d52a31e493c69e
Aleksander Jarkiewicz-Tretyn <s13077@pjwstk.edu.pl>
2020-10-30 12:23:13 +0100

    Added reading from file

---
+++ cw2/cw2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String file = "dane.csv";
+            using (StreamReader stream = new StreamReader(file))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] student = line.Split(',');
+                    var st = new Student
+                    {
+                        Imie = student[0],
+                        Nazwisko = student[1],
+                        Kierunek = student[2],
+                        Tryb = student[3],
+                        NrIndeksu = student[4],
+                        DataUrodzenia = student[5],
+                        Email = student[6],
+                        ImieMatki = student[7],
+                        ImieOjca = student[8]
+
+                    };
+                }
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
---
+++ cw2/cw2/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2
+{
+    class Student
+    {
+        public String Imie { get; set; }
+        public String Nazwisko { get; set; }
+        public String Kierunek { get; set; }
+        public String Tryb { get; set; }
+        public String NrIndeksu { get; set; }
+        public String DataUrodzenia { get; set; }
+        public String Email { get; set; }
+        public String ImieMatki { get; set; }
+        public String ImieOjca { get; set; }
+    }
+}
